cdf7b567c140990efbeb05e4c564f3e89f9d673f
Lucas Reed <lucasreed.dev@gmail.com>
2024-11-17 15:48:11 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+function copyLinktreeUrl() {
+    const url = "https://linktr.ee/yourusername";  
+    const icon = document.querySelector('.circle-icon');
+
+
+    icon.style.backgroundColor = '#3e1a75';  
+
+   
+    navigator.clipboard.writeText(url)
+        .then(() => {
+
+            setTimeout(() => {
+                icon.style.backgroundColor = '#000000';  
+                alert("Link copied to clipboard!");
+            }, 1000);  
+        })
+        .catch((err) => {
+            
+            console.error("Failed to copy: ", err);
+            icon.style.backgroundColor = '#dc3545';  
+            setTimeout(() => {
+                icon.style.backgroundColor = '#000000';  
+            }, 1000);
+        });
+}
 
