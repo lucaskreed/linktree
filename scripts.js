883bc9005eab5755c945a2f0529cf9e3e8e7a7e6
Lucas Reed <lucasreed.dev@gmail.com>
2024-11-17 16:57:57 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,23 +2,23 @@ function copyLinktreeUrl() {
     const url = "https://lucasreed1.github.io/linktree/";  
     const icon = document.querySelector('.circle-icon');
 
-    // Change the icon color to indicate action
+    
     icon.style.backgroundColor = '#3e1a75';  
 
     navigator.clipboard.writeText(url)
         .then(() => {
-            // Successfully copied: revert back and give feedback
+            
             setTimeout(() => {
                 icon.style.backgroundColor = '#000000';  
                 alert("Link copied to clipboard!");
             }, 1000);  
         })
         .catch((err) => {
-            // In case of failure, change to red and reset the color
+         
             console.error("Failed to copy: ", err);
-            icon.style.backgroundColor = '#dc3545';  // Red color for error
+            icon.style.backgroundColor = '#dc3545';  
             setTimeout(() => {
-                icon.style.backgroundColor = '#000000';  // Reset to black after 1 second
+                icon.style.backgroundColor = '#000000';  
             }, 1000);
         });
 }
